e1e4cb1ebcd8ef16319ebf7c812a904be9b9abc1
Alexandre Felix <alexandrefelix@Alexandres-iMac.local>
2018-07-15 15:28:03 +0200

    add comments

--- commands.c
+++ commands.c
@@ -11,19 +11,25 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+//envoie un fichier au client 
 void downl(int sock2, char buf[], char filename[], struct stat obj) {
+    //lecture formaté d'une string (charactere jusqu'a un espace ou tab, newline)
     sscanf(buf, "%s%s", filename, filename);
+    //recupere les infos du fichier 
     stat(filename, &obj);
+    // creer file descriptor du fichier 
     int fd = open(filename, O_RDONLY);
+    //recupere la taille du fichier
     size_t size = (size_t) obj.st_size;
 
     if(fd == -1) {
         printf("%s\n","No such file.");
         size = 0;
     }
-
+    //renvoie la taille du fichier 
     send(sock2, &size, sizeof(int), 0);
 
+    //charge le fichier en memoire 
     char file[size];
     int i = 0;
     FILE*f = fopen(filename,"r");
@@ -31,25 +37,30 @@ void downl(int sock2, char buf[], char filename[], struct stat obj) {
         file[i++] = (char) fgetc(f);
     file[i-1] = '\0';
     fclose(f);
-
+    //envoie le fichier 
     send(sock2, file, size, 0);
 }
 
+//recois un fichier depuis le client 
 void upld(int sock2, char buf[], char command[], char filename[], size_t size) {
     int c = 0, fd;
     sscanf(buf+strlen(command) + 1, "%s", filename);
     recv(sock2, &size, sizeof(int), 0);
 
+    //alloue la memoire et recois le fichier 
     char *f = malloc(size);
     recv(sock2, f, size, 0);
 
+    //écrit le fichier stocker en memoire 
     fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
     c = (int) write(fd, f, size);
     close(fd);
 
+    //renvoie le statut de l'ecriture 
     send(sock2, &c, sizeof(int), 0);
 }
 
+//recupere le pathname repertoire courant sur le serveur 
 void rpwd(int sock2, char buf[]) {
     system("pwd>temp.txt");
     int i = 0;
@@ -62,6 +73,7 @@ void rpwd(int sock2, char buf[]) {
     send(sock2, buf, 100, 0);
 }
 
+//renvoie le listing des fichiers et dossiers du WorDir
 void rls(int sock2, struct stat obj) {
     system("ls>temps.txt");
     stat("temps.txt",&obj);
@@ -77,16 +89,19 @@ void rls(int sock2, struct stat obj) {
     fclose(f);
     system("rm temps.txt");
 
+    //on envoie la taille du fichier temporaire 
     send(sock2, &size, sizeof(unsigned long),0);
-
+    //on envoie le contenu du fichier temporaire
     send(sock2, buf, size, 0);
 }
 
+//change le workdir sur le serveur 
 void rcd(int sock2, char buf[]) {
     int c = chdir(buf+4) == 0 ? 1 : 0;
     send(sock2, &c, sizeof(int), 0);
 }
 
+//obvious
 void quit(int sock2) {
     int i = 1;
     send(sock2, &i, sizeof(int), 0);
--- main.c
+++ main.c
@@ -10,11 +10,13 @@ int loop(int sock) {
     size_t size;
     char buf[128], command[8], filename[128];
 
+    //boucle de command
     accept:
     printf("%s\n", "Waiting for command.");
     recv(sock, buf, 128, 0);
     sscanf(buf, "%s", command);
     printf("%s\n", "Command received.");
+
     if(!strcmp(command, "rls")) {
         rls(sock, obj);
     } else if(!strcmp(command,"downl")) {
@@ -34,7 +36,9 @@ int loop(int sock) {
     goto accept;
 }
 
+//verify les credentials envoyés par le client 
 int verify_account(char* account, char* password) {
+    //ouvre le fichier contenant les compte 
     FILE *fp=fopen("accounts","r");
     short authenticated = 0;
     char tmp[512]={0x0};
@@ -42,6 +46,7 @@ int verify_account(char* account, char* password) {
     strcpy(p, ";");
     strcat(p, password);
 
+    //on cherche le compte correspondant au credentials passé en param
     while(fp != NULL && fgets(tmp, sizeof(tmp), fp) != NULL)
     {
         if(strstr(tmp, account) && strstr(tmp, p))
@@ -63,6 +68,7 @@ int main(int argc,char *argv[])
     int sock1, sock2;
     int k, len;
 
+    // creation socket
     sock1 = socket(AF_INET, SOCK_STREAM, 0);
 
     if(sock1 == -1)
@@ -74,12 +80,14 @@ int main(int argc,char *argv[])
     server.sin_port = htons(1977);
     server.sin_addr.s_addr = 0;
 
+    //binding du socket 
     k = bind(sock1,(struct sockaddr*)&server,sizeof(server));
     if(k == -1)
     {
         printf("Binding error");
         exit(1);
     }
+    //on passe le socket en ecoute pour recevoir la connection d'un client 
     k = listen(sock1,1);
     if(k == -1)
     {
@@ -87,11 +95,14 @@ int main(int argc,char *argv[])
         exit(1);
     }
 
+
     printf("%s\n","Waiting for client to connect...");
     len = sizeof(client);
+    //recoit la connection d'un client 
     sock2 = accept(sock1, (struct sockaddr*)&client, (socklen_t *) &len);
     printf("%s\n","Client connected...");
 
+    //etape d'identification du client par login/mdp
     recv(sock2, &buf, 128, 0);
     printf("%s\n", buf);
     if(strcmp(buf, "BONJ") == 0) {
@@ -99,12 +110,14 @@ int main(int argc,char *argv[])
         recv(sock2, &account, 128, 0);
 
         send(sock2, "PASSWD", 128, 0);
+        //annule la connection apres trois essais raté 
         for (int i = 0; i < 3; ++i) {
             recv(sock2, &password, 128, 0);
             if(verify_account(account, password)) {
                 send(sock2, "WELC", 128, 0);
                 return loop(sock2);
             } else {
+                //quand le mdp de passe rentré est incorrect
                 send(sock2, "WRONG", 128, 0);
             }
         }
